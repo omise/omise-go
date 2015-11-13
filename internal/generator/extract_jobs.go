f3a373f96d3fe66debd272ac3f0a1adad9d99816
Chakrit Wichian <service@chakrit.net>
2015-11-13 18:25:45 +0700

    Better String() generator.

--- internal/generator/extract_jobs.go
+++ internal/generator/extract_jobs.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"go/importer"
 	"go/types"
+	"strings"
 )
 
 func ExtractJobs(importpath string) ([]Job, error) {
@@ -12,23 +14,33 @@ func ExtractJobs(importpath string) ([]Job, error) {
 	}
 
 	scope := pkg.Scope()
-	listJob := &GenListJob{}
+	listJob, stringJob := &GenListJob{}, &GenStringJob{map[string][]string{}}
 	for _, name := range scope.Names() {
 		obj := scope.Lookup(name)
 		typ := obj.Type()
 
 		switch findClass(typ) {
 		case ModelStruct:
+			fmt.Println("MODEL: ", name)
+			struc := typ.(*types.Named).Underlying().(*types.Struct)
 			listJob.Names = append(listJob.Names, name)
+			stringJob.Fields[name] = collectFields(struc)
 		}
 	}
 
-	return []Job{listJob}, nil
+	fmt.Printf("%#v", stringJob)
+	return []Job{listJob, stringJob}, nil
 }
 
 func findClass(typ types.Type) (class Class) {
 	named, ok := typ.(*types.Named)
 	if !ok {
+		return Uninterested
+	}
+
+	name := named.Obj().Name()
+	switch name {
+	case "List", "Base":
 		return Uninterested
 	}
 
@@ -50,4 +62,27 @@ func findClass(typ types.Type) (class Class) {
 	}
 
 	return Uninterested
+}
+
+func collectFields(struc *types.Struct) []string {
+	result := []string{}
+
+	for i := 0; i < struc.NumFields(); i++ {
+		f, tag := struc.Field(i), struc.Tag(i)
+		fmt.Println(f.Name(), tag)
+
+		switch {
+		case f.Anonymous():
+			if named, ok := f.Type().(*types.Named); ok {
+				if struc_, ok := named.Underlying().(*types.Struct); ok {
+					result = append(result, collectFields(struc_)...)
+				}
+			}
+
+		case strings.Contains(tag, `pretty:""`):
+			result = append(result, f.Name())
+		}
+	}
+
+	return result
 }
