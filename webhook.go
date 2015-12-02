74b12c95432895cd0f27284b1a24a32ca153b84a
Chakrit Wichian <service@chakrit.net>
2015-12-02 17:58:13 +0700

    HTTP Handler for receiving webhooks.

---
+++ webhook.go
@@ -0,0 +1,41 @@
+package omise
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// EventHandler is an interface for handling events from the webhook http.Handler.
+type EventHandler interface {
+	HandleEvent(http.ResponseWriter, *http.Request, *Event)
+}
+
+// EventHandlerFunc lets you use a plain function as an EventHandler type.
+type EventHandlerFunc func(http.ResponseWriter, *http.Request, *Event)
+
+func (f *EventHandlerFunc) HandleEvent(resp http.ResponseWriter, req *http.Request, event *Event) {
+	f(resp, req, event)
+}
+
+type webhookHTTPHandler struct {
+	eventHandler EventHandler
+}
+
+func (h *webhookHTTPHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	event := &omise.Event{}
+	if e := json.NewDecoder(req.Body).Decode(event); e != nil {
+		http.Error(resp, e.Error(), http.StatusBadRequest)
+		return
+	}
+
+	h.eventHandler.HandleEvent(req, resp, event)
+}
+
+// WebhookHTTPHandler creates an http.Handler that you can use to receive Omise's webhook
+// API calls for events. The returned handler will automatically consume the request body
+// and unmarshals an Event object from JSON for you.
+//
+// See https://www.omise.co/api-webhooks for more information.
+func WebhookHTTPHandler(handler EventHandler) http.Handler {
+	return &webhookHTTPHandler{handler}
+}
