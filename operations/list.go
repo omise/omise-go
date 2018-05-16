67687ee3dc5c55ca2cd8c14261cdf17fc6237420
Weerasak Chongnguluam <singpor@gmail.com>
2018-05-16 11:05:41 +0700

    Move custom struct for marshal json to MarshalJSON method

--- operations/list.go
+++ operations/list.go
@@ -20,24 +20,23 @@ type List struct {
 	From   time.Time      `json:"-"`
 	To     time.Time      `json:"-"`
 	Order  omise.Ordering `json:"order,omitempty"`
-
-	NullableFrom *time.Time `json:"from,omitempty"`
-	NullableTo   *time.Time `json:"to,omitempty"`
 }
-
-type optionalFieldList List
 
 // MarshalJSON List type
 func (l List) MarshalJSON() ([]byte, error) {
-	l.SetOptionalField()
-	return json.Marshal(optionalFieldList(l))
-}
-
-func (l *List) SetOptionalField() {
+	type Alias List
+	params := struct {
+		Alias
+		PFrom *time.Time `json:"from,omitempty"`
+		PTo   *time.Time `json:"to,omitempty"`
+	}{
+		Alias: Alias(l),
+	}
 	if !l.From.IsZero() {
-		l.NullableFrom = &l.From
+		params.PFrom = &l.From
 	}
 	if !l.To.IsZero() {
-		l.NullableTo = &l.To
+		params.PTo = &l.To
 	}
+	return json.Marshal(params)
 }
