8c992b65d071cdd1c86824bd1549ce23e5554595
hardy <hardy@omise.co>
2019-08-19 09:55:33 +0700

    No bundled certificate
    Removed the CA chain as hard coded because we don't want to maintain
    that dependency. Client will try and get CA chain
    from system instead. This creates a external dependency, but this is how
    it should be.
    
    System paths: https://golang.org/src/crypto/x509/root_linux.go
    
    Note: The pinning / bundle was for security, ( only certificates from
    same CA would work. ) But not a good control and can create more issues
    then solves.

--- transport.go
+++ transport.go
@@ -2,28 +2,14 @@ package omise
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"net/http"
-
-	"github.com/omise/omise-go/internal/creds"
 )
 
 var transport *http.Transport
 
 func init() {
-	certbytes, err := creds.Asset("ca_certificates.pem")
-	if err != nil {
-		panic(err)
-	}
-
-	pool := x509.NewCertPool()
-	if !pool.AppendCertsFromPEM(certbytes) {
-		panic("failed to load omise.co domain certificates.")
-	}
-
 	transport = &http.Transport{
 		TLSClientConfig: &tls.Config{
-			RootCAs:    pool,
 			MinVersion: tls.VersionTLS12,
 		},
 	}
