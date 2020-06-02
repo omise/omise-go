feb8215b6f926fb1a067686025343551f2aa0532
Marut K <marut@omise.co>
2020-06-02 18:05:01 +0700

    Add type alias and rename based struct for List

--- v2/forex.go
+++ v2/forex.go
@@ -3,6 +3,7 @@ package omise
 // Forex represents Forex object.
 // See https://www.omise.co/forex-api for more information.
 type Forex struct {
+	BaseModel
 	Base string `json:"base"`
 	Location string `json:"location"`
 	Quote string `json:"quote"`
--- v2/go.mod
+++ v2/go.mod
@@ -4,4 +4,6 @@ go 1.12
 
 require (
 	github.com/stretchr/testify v1.6.0
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/tools/gopls v0.4.1 // indirect
 )
