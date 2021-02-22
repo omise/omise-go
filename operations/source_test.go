dbb6d4b161d975511f1f97f8c3faf03d33abafe3
hpariwat <61586978+hpariwat@users.noreply.github.com>
2021-02-22 09:58:23 +0700

    Support extra fields for Source API

--- operations/source_test.go
+++ operations/source_test.go
@@ -5,6 +5,7 @@ import (
 
 	omise "github.com/omise/omise-go"
 	"github.com/omise/omise-go/internal/testutil"
+	"github.com/omise/omise-go/operations"
 	. "github.com/omise/omise-go/operations"
 	r "github.com/stretchr/testify/require"
 )
@@ -19,7 +20,14 @@ func TestSource(t *testing.T) {
 	client.MustDo(source, &RetrieveSource{SourceID: SourceID})
 	r.Equal(t, SourceID, source.ID)
 
-	source = &omise.Source{}
-	client.MustDo(source, &CreateSource{})
-	r.Equal(t, SourceID, source.ID)
+	exampleSource, createSource := &omise.Source{}, &operations.CreateSource{
+		Type:     "fpx",
+		Amount:   2000,
+		Currency: "myr",
+		Email:    "example@omise.co",
+		Bank:     "ocbc",
+	}
+
+	client.MustDo(exampleSource, createSource)
+	r.Equal(t, SourceID, exampleSource.ID)
 }
