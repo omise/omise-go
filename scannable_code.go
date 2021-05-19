1d3027b29a5173bbb7b867d183cea6b4c29984d1
som-m <chanapa.pntt@hotmail.com>
2021-05-19 21:10:38 +0700

    fix comment

--- scannable_code.go
+++ scannable_code.go
@@ -1,6 +1,6 @@
 package omise
 
-// Scannable code represents Omise's barcode object.
+// ScannableCode represents Omise's barcode object.
 // See https://www.omise.co/sources-api for more information.
 type ScannableCode struct {
 	Object string    `json:"object"`
