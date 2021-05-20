b5e2267317bebf0b14a9d1aec376c767390c2822
Natthawut Haematulin <natthawut1991@gmail.com>
2021-05-20 21:31:31 +0700

    feat: apply base fields in source model

--- source.go
+++ source.go
@@ -3,10 +3,7 @@ package omise
 // Source represents Omise's source object.
 // See https://www.omise.co/source-api for more information.
 type Source struct {
-	Object        string         `json:"object"`
-	ID            string         `json:"id"`
-	Live          bool           `json:"livemode"`
-	Location      *string        `json:"location"`
+	Base
 	Type          string         `json:"type"`
 	Flow          string         `json:"flow"`
 	Amount        int64          `json:"amount"`
