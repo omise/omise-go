c9f1acae3c69e3bec1a338e619f036977f7e6b34
Natthawut Haematulin <natthawut1991@gmail.com>
2021-05-20 21:59:08 +0700

    feat: support bill payment

--- source.go
+++ source.go
@@ -4,9 +4,11 @@ package omise
 // See https://www.omise.co/source-api for more information.
 type Source struct {
 	Base
-	Type          string         `json:"type"`
-	Flow          string         `json:"flow"`
-	Amount        int64          `json:"amount"`
-	Currency      string         `json:"currency"`
-	ScannableCode *ScannableCode `json:"scannable_code"`
+	Type                     string         `json:"type"`
+	Flow                     string         `json:"flow"`
+	Amount                   int64          `json:"amount"`
+	Currency                 string         `json:"currency"`
+	ScannableCode            *ScannableCode `json:"scannable_code"`
+	References               *References    `json:"references"`
+	ZeroInterestInstallments bool           `json:"zero_interest_installments"`
 }
