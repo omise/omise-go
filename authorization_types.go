704c2208b822c74d7d86fa458567aa39276e91e5
AnasNaouchi <anas.n@opn.ooo>
2023-09-21 15:53:58 +0700

    Support partial capture

---
+++ authorization_types.go
@@ -0,0 +1,10 @@
+package omise
+
+// AuthorizationType represents an enumeration of a possible authorization type of a Charge object.
+type AuthorizationType string
+
+// AuthorizationType can be one of the following list of constants:
+const (
+	PreAuth     AuthorizationType = "pre_auth"
+	FinalAuth    AuthorizationType = "final_auth"
+)
