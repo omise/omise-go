11c30506902e263744a5b932bdafc6b67cd4c850
Tyler <sai.a@opn.ooo>
2026-02-16 10:30:23 +0700

    sunset internet banking (#97)
    ## Description
    - remove internet banking source types 
    
    
    ### Documentation (if any)
    n/a
    
    
    ## Business impact (if any)
    - Internet bankings are no longer supported

--- offsite_types.go
+++ offsite_types.go
@@ -5,9 +5,5 @@ type OffsiteTypes string
 
 // OffsiteTypes can be one of the following list of constants:
 const (
-	InternetBankingSCB OffsiteTypes = "internet_banking_scb"
-	InternetBankingBBL OffsiteTypes = "internet_banking_bbl"
-	InternetBankingKTB OffsiteTypes = "internet_banking_ktb"
-	InternetBankingBAY OffsiteTypes = "internet_banking_bay"
-	Alipay             OffsiteTypes = "alipay"
+	Alipay OffsiteTypes = "alipay"
 )
